d062881b3ec5f2d27cf6b5eb17a49f34d07c3923
rafaeltod <124226801+rafaeltod@users.noreply.github.com>
2023-07-01 21:28:29 +0000

    pao de queijo finalizado

--- ListaPoo04B/2552.py
+++ ListaPoo04B/2552.py
@@ -1,27 +1,32 @@
-n, m = map(int, input().split())
-matriz = []
+while True:
+  try:
+    
+    n, m = map(int, input().split())
+    matriz = []
 
-for k in range(n + 2):
-  matriz.append([0] * (m + 2))
+    for k in range(n + 2):
+      matriz.append([0] * (m + 2))
 
-for k in range(n):
-  linha = list(map(int, input().split())) 
-  for j in range(m):
-    matriz[k+1][j+1] = linha[j]
+    for k in range(n):
+      linha = list(map(int, input().split())) 
+      for j in range(m):
+        matriz[k+1][j+1] = linha[j]
 
-for i in range(1, n + 1):
-  for j in range(1, m + 1):
-    if matriz[i][j] == 1:
-      print(9, end='')
-    else:
-      soma = 0
-      if matriz[i-1][j]:
-        soma += 1
-      if matriz[i][j-1] == 1:
-        soma += 1
-      if matriz[i+1][j] == 1:
-        soma += 1
-      if matriz[i][j+1] == 1:
-        soma += 1
-      print(soma, end='')
-  print()
+    for i in range(1, n + 1):
+      for j in range(1, m + 1):
+        if matriz[i][j] == 1:
+          print(9, end='')
+        else:
+          soma = 0
+          if matriz[i-1][j]:
+            soma += 1
+          if matriz[i][j-1] == 1:
+            soma += 1
+          if matriz[i+1][j] == 1:
+            soma += 1
+          if matriz[i][j+1] == 1:
+            soma += 1
+          print(soma, end='')
+      print()
+  except EOFError:
+    break
